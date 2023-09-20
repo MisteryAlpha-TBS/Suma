8af652ae8a244844fbf6261e89cc52970c87af05
MisteryAlpha-TBS <pipopipopi1928@gmail.com>
2023-09-20 16:05:52 -0600

    Suma de dos números

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿int num1 = 0;
+int num2 = 0;
+int resultado = 0;
+
+Console.WriteLine("Ingrese el primer numero a sumar");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ingrese el segundo numero a sumar");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+resultado = num1 + num2;
+Console.WriteLine("El resultado es: " +  resultado);
